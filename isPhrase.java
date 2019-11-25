906ce2d0a4f4cd2b828f69b7dd9b9365ee4a0b5d
Josh Lawless <jlawless0048@myduneland.org>
2019-11-25 12:31:38 -0600

    Done some stuff

--- isPhrase.java
+++
@@ -1,6 +0,0 @@
-public class isPhrase
-{
-    public void isPhrase(String str){
-        
-    }
-}
---
+++ methods.java
@@ -0,0 +1,28 @@
+import java.io.*; 
+import java.util.Scanner;
+
+public class methods
+{
+    public void isPhrase(String str) throws IOException {    
+    Scanner reader = new Scanner(new File("texts/phrases.txt"));
+    Scanner readerTwo = new Scanner(new File("texts/phrases.txt")); 
+    int arrayCount = 0;
+    while(reader.hasNext()){
+        reader.nextLine();
+        arrayCount++;
+    }   
+    String[] phrase = new String[arrayCount];
+    int count = 0;
+    while (readerTwo.hasNext()){
+        String stringuno = readerTwo.nextLine();
+        phrase[count] = stringuno;
+        count++;
+    }
+    
+    for(String element : phrase){
+        if(str.equals(element)){
+            
+        } 
+    }
+   }
+}
