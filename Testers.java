8fa8e4e419119d8c86beb4e42672595754e4e96c
Josh Lawless <jlawless0048@myduneland.org>
2019-12-04 12:57:17 -0600

    added random

--- Testers.java
+++ Testers.java
@@ -1,5 +1,6 @@
 import java.io.*; 
 import java.util.Scanner;
+import java.util.Random;
 
 public class Testers
 {
@@ -37,25 +38,47 @@ public class Testers
         reader.nextLine();
         arrayCount++;
     }   
-    String[] phrase = new String[arrayCount];
+    String[] key = new String[arrayCount];
     int count = 0;
     while (readerTwo.hasNext()){
         String stringuno = readerTwo.nextLine();
-        phrase[count] = stringuno;
+        key[count] = stringuno;
         count++;
     }
     
-    for(String element : phrase){
+    for(String element : key){
         if(str.contains(element)){
             return element + "_true";
         } 
     }
     return "false";
    }
-   
-  // public String responce(String str) throws IOException {
-    //   if(str == "false")
-            
-    //}
+  
+   public String responceRandom() throws IOException{
+    Scanner reader = new Scanner(new File("texts/responces/random.txt"));
+    Scanner readerTwo = new Scanner(new File("texts/responces/random.txt")); 
+    Random rand = new Random();
+    int arrayCount = 0;
+    while(reader.hasNext()){
+        reader.nextLine();
+        arrayCount++;
+    }   
+    String[] resRan = new String[arrayCount];
+    int count = 0;
+    while (readerTwo.hasNext()){
+        String stringuno = readerTwo.nextLine();
+        resRan[count] = stringuno;
+        count++;
+    }
+    
+      return resRan[rand.nextInt(count)];
+   }
+    
+   public String responce(String str) throws IOException {
+       if(str == "false"){
+           return responceRandom();
+        }
+       return "";    
+    }
     
 }
