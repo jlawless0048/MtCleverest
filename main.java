b93bf89fbd3380f117fa024196771815e569dc27
drew <dmalapanes0148@myduneland.org>
2019-12-04 12:14:32 -0600

    v

---
+++ main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/**
+ *MAIN
+ */
+public class main{
+	public static void main(String[] args)
+	{
+		main talker = new main();
+		
+		System.out.println ("Hello...");
+		Scanner res = new Scanner (System.in);
+		String response = res.nextLine();
+		response.toLowerCase();
+		
+		while (!response.equals("bye") || !response.equals("goodbye")
+		|| !response.equals("end"))
+		{
+			System.out.println("");
+			response = res.nextLine();
+		}
+	}
+}
